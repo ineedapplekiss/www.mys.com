957987f46ce953ab32492ac7378c7233a983bb7f
shang home <9212219@163.com>
2013-06-15 20:48:28 +0800

    del template

--- template/september1/images/33.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function(){
-				jQuery('.boxgrid.captionfull').hover(function(){
-					jQuery(".cover", this).stop().animate({top:'125px'},{queue:false,duration:125});
-				}, function() {
-					jQuery(".cover", this).stop().animate({top:'162px'},{queue:false,duration:125});
-				});
-			});
