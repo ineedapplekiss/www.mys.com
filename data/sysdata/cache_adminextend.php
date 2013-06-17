a1709eb1cb288091b09798aea473637afcd32ae8
shang <9212219@163.com>
2013-06-17 14:53:05 +0800

    rm sysdata

--- data/sysdata/cache_adminextend.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 2a5d4eadd19270516c9c0e1546223337
-
-$adminextend = array (
-  0 => 'cloud.php',
-);
-?>
--- data/sysdata/cache_pluginsetting.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 6d5d9668fdb2521b31a55f68c365093d
-
-$pluginsetting = array (
-);
-?>
